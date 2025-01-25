c99399d827948d7efd8a0a4f6058960af1254115
aaron <aaron.a@me.com>
2025-01-24 20:51:47 -0600

    started building out the package
    Signed-off-by: aaron <aaron.a@me.com>

--- Sources/peruna/peruna.swift
+++ Sources/peruna/peruna.swift
@@ -1,2 +1,43 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+import MetalKit
+#if canImport(PlaygroundSupport)
+import PlaygroundSupport
+#endif
+
+class MetalContext {
+    let device: MTLDevice?
+    let view: MTKView
+    let frame: CGRect
+    
+    @MainActor init() {
+        guard let device = MTLCreateSystemDefaultDevice() else { fatalError("GPU not found") }
+        self.device = device
+        self.frame = CGRect(x: 0, y: 0, width: 600, height: 600)
+        self.view = MTKView(frame: frame, device: device)
+        self.view.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 0.8, alpha: 1.0)
+    }
+    
+    @MainActor public func changCanvasBG(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        self.view.clearColor = MTLClearColor(red: r, green: g, blue: b, alpha: a)
+    }
+    
+    @MainActor public func changeCanvasSize(w: CGFloat, h: CGFloat) {
+        self.view.frame = CGRect(x: 0, y: 0, width: w, height: h)
+    }
+}
+
+@MainActor let metalContext = MetalContext()
+
+@MainActor func setup( _ userSetupMethod: () -> Void) {
+    #if canImport(PlaygroundSupport)
+    PlaygroundPage.current.liveView = ctx.view
+    #endif
+    
+    userSetupMethod()
+}
+
+func draw(_ userDrawMethod: () -> Void) {
+    userDrawMethod()
+}
